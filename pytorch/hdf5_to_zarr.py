ee917cff45bc1bce2777df03b2ce7b0f4227cd66
andreas <andreas.tenpas@gmail.com>
2019-05-15 18:33:44 -0400

    training with zarr files

--- pytorch/hdf5_to_zarr.py
+++ pytorch/hdf5_to_zarr.py
@@ -3,20 +3,13 @@ import numpy as np
 import sys
 import zarr
 
+print('Loading database ...')
 h5_file = h5py.File(sys.argv[1])
 images = h5_file.get('images')
 labels = h5_file.get('labels')
 
 root = zarr.open(sys.argv[2], mode='w')
-images_out = root.create_dataset('images', shape=images.shape, dtype='float32')
-labels_out = root.create_dataset('labels', shape=labels.shape, dtype='int32')
-
-images_out[:] = images[:]
-labels_out[:] = labels[:]
-
-#zarr_out = zarr.open(sys.argv[2], mode='w', shape=images.shape)
-#for i in range(images.shape[0]):
-#    print i
-#    zarr_out[i] = images[i]
-#    if i % 1000 == 0:
-#        print(i, images.shape[0])
+print('Writing images of shape', images.shape, '...')
+images_out = root.create_dataset('images', data=images, dtype=images.dtype) #, chunks=(5000,) + images.shape[1:])
+print('Writing labels of shape', labels.shape, '...')
+labels_out = root.create_dataset('labels', data=labels, dtype=labels.dtype)
